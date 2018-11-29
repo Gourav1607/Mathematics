4be8d6df901204c239ce1351f1d15358979ca7ea
Gourav Siddhad <gourav1607@gmail.com>
2018-11-29 19:12:11 +0530

    added Numerical Methods

---
+++ Numerical Methods/C++ - Gauss Seidel/gaussseidel.cpp
@@ -0,0 +1,58 @@
+#include "iostream"
+#include "cstdlib"
+#include "iomanip"
+#include "cmath"
+#define SETW_DEF 5
+using namespace std;
+
+int main() {
+	system("clear");
+	int n;
+	cout << "\n Enter Square Matrix Size : ";
+	cin >> n;
+
+	float mat[n][n], c[n], guess[n], x[n], e[n];
+
+	cout << "\n Enter Matrix : \n";
+	for (int i = 0; i < n; i++) {		// Enter Matrix
+		for (int j = 0; j < n; j++) {
+			cout << " Enter Element MAT[" << i << "][" << j << "] : ";
+			cin >> mat[i][j];
+		}
+	}
+	cout << "\n Enter Augmented Matrix Elements : \n";
+	for (int i = 0; i < n; i++) {		// Enter C (Augmented Matrix Elements)
+		cout << " Enter Element C[" << i << "] : ";
+		cin >> c[i];
+	}
+	cout << "\n Enter X (Guess) : \n";
+	for (int i = 0; i < n; i++) {		// Enter Guess
+		cout << " Enter Element Guess[" << i << "] : ";
+		cin >> guess[i];
+	}
+
+	int z = 0;
+	do {
+		for (int i = 0; i < n; i++) {		// Calcualting X[i]
+			x[i] = c[i];
+			for (int j = 0; j < n; j++) {
+				if (i != j)
+					x[i] -= mat[i][j] * guess[j];
+			}
+			x[i] /= mat[i][i];
+			e[i] = fabs(guess[i] - x[i]) / x[i];
+			guess[i] = x[i];		// Changing Guess Value For New Calculated X
+		}
+
+		cout << "\n X[i] Elements For Iteration " << z + 1 << " : \n";
+		for (int i = 0; i < n; i++) {
+			cout << "   X[" << i << "] : " << setw(SETW_DEF) << x[i];
+			cout << "   Guess[" << i << "] : " << setw(SETW_DEF) << guess[i];
+			cout << "   Error[" << i << "] : " << setw(SETW_DEF) << e[i];
+			cout << endl;
+		}
+		z++;
+	} while ((e[0] > 0.0001 || e[1] > 0.0001 || e[2] > 0.0001) && z < 100);
+	cout << endl;
+	return 0;
+}
---
+++ Numerical Methods/C++ - Newton Raphson/newtoncos.cpp
@@ -0,0 +1,60 @@
+#include "iostream"
+#include "cmath"
+#include "cstdlib"
+using namespace std;
+
+double factorial(double x) {
+	double y = 1;
+	if (x == 0)
+		return 1;
+	for (int i = 1; i <= x; i++)
+		y *= i;
+	return y;
+}
+
+double fdas(double x) { //sine series
+	double sum = 0;
+	int k = 2;
+	for (int i = 1; i <= 40;) {
+		sum += ((pow(-1, k) * (pow(x, i))) / factorial(i));
+		k++;
+		i += 2;
+	}
+	return sum;
+}
+
+double f(double x) { //cosine series
+	double sum = 0;
+	int k = 2;
+	for (int i = 0; i <= 40;) {
+		sum += ((pow(-1, k) * (pow(x, i))) / factorial(i));
+		k++;
+		i += 2;
+	}
+	return sum;
+}
+
+int main() {
+	double x, y;
+	int i = 0;
+	double sig = 0.000001;
+	cout << "Enter guess : \n";
+	cin >> x;
+	if (f(x) == 0)
+		cout << "root is : " << x << endl;
+
+	do {
+		double y;
+		if (i == 20)
+			break;
+		y = x - (f(x) / (-1) * fdas(x));
+		cout << " x = " << x << "\t| y = " << y << "\t| f(x) = " << f(x)
+				<< "\t| f(y) = " << f(y) << endl;
+		if (f(y) == 0)
+			break;
+		else
+			x = y;
+		i++;
+	} while (abs(x - y) > 0.000001);
+	return 0;
+}
